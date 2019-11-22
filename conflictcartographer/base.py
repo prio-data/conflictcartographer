844fd260aa69664b3c0df539a27763d95344d6be
Peder Landsverk <pglandsverk@gmail.com>
2019-11-22 15:34:19 +0100

    Added email interval to base config
    Important to avoid key errors.

--- conflictcartographer/base.py
+++ conflictcartographer/base.py
@@ -95,3 +95,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
+
+EMAIL_INTERVAL = 1
