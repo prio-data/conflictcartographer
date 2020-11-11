8de9840ab7c3a5f04f931cbbfe1713f81c6856d6
peder2911 <pglandsverk@gmail.com>
2020-11-11 16:49:26 +0100

    added initial consent clicky thing

--- frontend/src/accounts.js
+++ frontend/src/accounts.js
@@ -1,4 +1,15 @@
 import "@/sass/accounts.sass"
+import Vue from "vue"
+import VueCookies from "vue-cookies"
+import ConsentModal from "@/components/ConsentModal"
 
 let regbase = document.querySelector("#regbase")
 regbase.style.display = "block"
+
+Vue.use(VueCookies)
+window.$cookies.config("7d")
+
+new Vue({
+   el: "#consentmodal",
+   render: h=>h(ConsentModal)
+}).$mount()
---
+++ frontend/src/components/ConsentModal.vue
@@ -0,0 +1,77 @@
+<template>
+   <div id="root">
+      <div v-if="!accepted" id="modal">
+         <div id="consentcard">
+            <h1>Hey!</h1>
+            <p id="infotext">
+               By continuing to use this page, you consent to the storage of
+               your contact information in our database. <br> Your information will
+               be treated confidentially. 
+            </p>
+            <button v-on:click="accept">That's ok!</button>
+         </div>
+      </div>
+   </div>
+</template>
+
+<style scoped>
+body {
+   padding: 0;
+   margin: 0;
+}
+
+button {
+   margin: 20px;
+   font-size: 20px;
+   width: 180px;
+   height: 80px;
+}
+
+button:hover{
+   background-color: white;
+   cursor: pointer;
+}
+
+#modal {
+   position: absolute;
+   display: grid;
+   place-items: center;
+   top: 0;
+   left: 0;
+   height: 100vh;
+   width: 100vw;
+   background: rgba(200,200,200,0.8);
+}
+#consentcard {
+   display: grid;
+   place-items: center;
+   grid-gap: 10px;
+   text-align: center;
+   background: #f0f0f0;
+   border-radius: 12px;
+   font-size: 20px;
+   padding: 80px;
+}
+</style>
+<script>
+export default {
+   name: "ConsentModal",
+   data(){
+      return {
+         accepted: false
+      }
+   },
+   mounted(){
+      let consent = this.$cookies.get("consent")
+      if(consent){
+         this.accepted = true
+      }
+   },
+   methods: {
+      accept(){
+         this.$cookies.set("consent",true)
+         this.accepted = true
+      }
+   }
+}
+</script>
