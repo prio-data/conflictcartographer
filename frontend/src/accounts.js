702fdba4914377650267c0e32afec244e1a13e07
peder2911 <pglandsverk@gmail.com>
2020-11-11 16:49:39 +0100

    Small sass change

--- frontend/src/accounts.js
+++ frontend/src/accounts.js
@@ -3,9 +3,6 @@ import Vue from "vue"
 import VueCookies from "vue-cookies"
 import ConsentModal from "@/components/ConsentModal"
 
-let regbase = document.querySelector("#regbase")
-regbase.style.display = "block"
-
 Vue.use(VueCookies)
 window.$cookies.config("7d")
 
@@ -13,3 +10,6 @@ new Vue({
    el: "#consentmodal",
    render: h=>h(ConsentModal)
 }).$mount()
+
+let regbase = document.querySelector("#regbase")
+regbase.style.display = "block"
