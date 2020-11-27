b9a8a6cb739a8389c8a13f6915ef1fdc01f6fd6a
peder2911 <pglandsverk@gmail.com>
2020-11-27 17:06:24 +0100

    Added pagination to admin views
    Also fixed an errant router assignment

--- app/api/admin_views.py
+++ app/api/admin_views.py
@@ -6,14 +6,30 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
 from rest_framework.permissions import IsAdminUser
 from rest_framework.routers import DefaultRouter
+from rest_framework.pagination import PageNumberPagination
 
 from django_filters import rest_framework as filters
 
 from api.models import Shape,Feedback,Country
 
 
+# ========================================================
+
 class AdminModelViewSet(ModelViewSet):
     permission_classes=[IsAdminUser]
+
+
+class SmallAdminPagination(PageNumberPagination):
+    page_size = 25 
+    page_size_query_param = 'page_size'
+    max_page_size = 25 
+
+class LargeAdminPagination(PageNumberPagination):
+    page_size = 100 
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+# ========================================================
 
 class UserSerializer(ModelSerializer):
     class Meta:
@@ -23,13 +39,15 @@ class UserSerializer(ModelSerializer):
 class UserViewSet(AdminModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer 
+    pagination_class = LargeAdminPagination
+
+# ========================================================
 
 class ShapeFilter(filters.FilterSet):
     after = filters.DateFilter(field_name="date",lookup_expr="gte")
     before = filters.DateFilter(field_name="date",lookup_expr="lte")
     author = filters.ModelChoiceFilter(field_name="author",queryset=User.objects.all())
     gwno = filters.ModelChoiceFilter(field_name="country",queryset=Country.objects.all())
-
 
 class ShapeSerializer(ModelSerializer):
     class Meta:
@@ -41,6 +59,9 @@ class ShapeViewSet(AdminModelViewSet):
     serializer_class = ShapeSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ShapeFilter
+    pagination_class = LargeAdminPagination
+
+# ========================================================
 
 class CountrySerializer(ModelSerializer):
     class Meta:
@@ -52,8 +73,25 @@ class CountryViewset(AdminModelViewSet):
     serializer_class = CountrySerializer 
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ["active"] 
+    pagination_class = LargeAdminPagination
+
+# ========================================================
+
+class FeedbackSerializer(ModelSerializer):
+    class Meta:
+        model = Feedback 
+        fields = ["message","author","stars"]
+
+class FeedbackViewset(AdminModelViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer 
+    filter_backends = (filters.DjangoFilterBackend,)
+    pagination_class = SmallAdminPagination 
+
+# ========================================================
 
 router = DefaultRouter()
 router.register(r"shapes",ShapeViewSet,basename="adminshape")
 router.register(r"users",UserViewSet,basename="user")
 router.register(r"countries",CountryViewset,basename="admincountry")
+router.register(r"feedback",FeedbackViewset,basename="feedback")
