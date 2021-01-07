fc07c42b814471f1102fbc7780e967a634566af3
peder2911 <pglandsverk@gmail.com>
2021-01-07 15:58:51 +0100

    Admin backdoor for closed status
    Makes it possible to test changes on prod without opening the site.

--- app/closedMiddleware/middleware.py
+++ app/closedMiddleware/middleware.py
@@ -5,8 +5,14 @@ from django.http import HttpResponse
 
 def check_active(get_response):
     def middleware(request):
+
+        closed = not settings.ACTIVE
+        nonloop = not request.path == "/closed/"
+        adminpage = request.path.startswith("/admin/")
+        staff = request.user.is_staff
+
         try:
-            if not request.path == "/closed/" and not settings.ACTIVE:
+            if closed and nonloop and not staff and not adminpage: 
                     return redirect("closed")
             return get_response(request)
         except Exception as e:
