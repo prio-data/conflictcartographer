5fe678f881aa01e02095a5b2caa9303a625a5d94
peder2911 <pglandsverk@gmail.com>
2021-02-03 17:22:46 +0100

    Mocking requests for the loader-router

--- frontend/src/components/LoadingRouter.vue
+++ frontend/src/components/LoadingRouter.vue
@@ -4,8 +4,11 @@ based on their current participation status. The routing follows a
 hand-holding strategy designed to maximize participation.
 -->
 <template>
-   <div>
-   LOADING
+   <div id="root">
+      <div class="card">
+         <h1>Loading...</h1>
+         <p class="status">{{ status }}</p>
+      </div>
    </div>
 </template>
 <style scoped>
@@ -15,31 +18,49 @@ div {
    display: grid;
    place-items: center;
 }
+.card {
+   height: 200px;
+}
 </style>
 <script>
+import Spinner from "@/components/widgets/Spinner"
 
 export default {
+   components: {Spinner},
+
+   data(){
+      return {
+         status: "Doing some stuff"
+      }
+   },
+
    mounted(){
       // Figure out where do go by doing some queries
       // TODO
       // Implement stuff in backend (requests)
       // Add routing (see log statements)
 
+      let SIM_DELAY = ()=>{return 250 + (250*Math.random())}
+
+      this.status = "Checking assigned countries"
       this.$store.state.api.get.rel("assigned")
-      Promise.resolve({data:[1,2,3]})
+      new Promise(r=>setTimeout(r,SIM_DELAY(),{data:[1,2,3]}))
+
          .then((assigned_countries)=>{
             if(assigned_countries.data.length == 0){
-               console.log("Routing to cselect")
+               this.$router.push("/assign")
                return true
             } else {
                return false
             }})
+
          .then((done)=>{
             if(!done){
-               return Promise.resolve({data:{status:"finished"}})//this.$store.state.api.get.rel("next")
+               this.status = "Checking status of assigned"
+               return new Promise(r=>setTimeout(r,SIM_DELAY(),{data:{status:"finished"}}))//this.$store.state.api.get.rel("next")
                   .then((next)=>{
                      if(next.data.status == "active"){
-                        console.log("Routing to next project")
+                        // this.$router.push(`/ctry/${next.gwno}`)
                         return true
                      } else {
                         return false
@@ -48,16 +69,18 @@ export default {
             } else {
                return done 
             }})
+
          .then((done)=>{
             if(!done){
-               return Promise.resolve({data:{meta:{foo:"bar"},prompted:true}})//this.$store.state.api.get.rel("profile")
+               this.status = "Checking profile status"
+               return new Promise(r=>setTimeout(r,SIM_DELAY(),{data:{meta:{foo:"bar"},prompted:true}}))//this.$store.state.api.get.rel("profile")
                   .then((profile)=>{
 
                      let hasmeta = Object.keys(profile.data.meta).length > 0
                      let prompted = profile.data.prompted
 
                      if(!(hasmeta | prompted)){
-                        console.log("Routing to metadata select")
+                        this.$router.push("/profile")
                         return true
                      } else {
                         return false
@@ -66,12 +89,15 @@ export default {
             } else {
                return done
             }})
+
          .then((done)=>{
             if(!done){
-               console.log("Routing to menu")
+               this.status = "Proceeding to menu"
+               this.$router.push("/menu")
             } else {
             }
          })
+
    }
 }
 </script>
