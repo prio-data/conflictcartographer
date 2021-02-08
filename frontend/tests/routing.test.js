85b1bd8ddd876b446643d350ad83c398d0b42076
peder2911 <pglandsverk@gmail.com>
2021-02-08 12:11:04 +0100

    Tests
    Started writing some tests
    
    This Leaflet-setup will be a little harder to test, so I'm putting it
    off. The logic is quite simple though, so it should be fine.

---
+++ frontend/tests/routing.test.js
@@ -0,0 +1,98 @@
+//jest.mock("@/util/debounce")
+
+import {mount} from "@vue/test-utils"
+import flushPromises from "flush-promises"
+import {Api} from "@/testutils"
+import {mock_countries} from "@/mocking"
+
+import LoadingRouter from "@/components/LoadingRouter"
+
+function mockRouter(){
+   this.path = undefined
+   this.push = (path)=>{
+      this.path = path
+   }
+}
+
+let respres=[
+   {
+      responses:{
+         get:{
+            "profile/assigned":{
+               countries:[]
+            },
+            "profile/unfulfilled":{
+               countries:[]
+            },
+            "profile/exists":{
+               profile: false 
+            },
+         },
+      },
+      result_path: "/assign"
+   },
+   {
+      responses:{
+         get:{
+            "profile/assigned":{
+               countries:[mock_countries(3)]
+            },
+            "profile/unfulfilled":{
+               countries:[mock_countries(3)]
+            },
+            "profile/exists":{
+               profile: false 
+            },
+         },
+      },
+      result_path: "/progress"
+   },
+   {
+      responses:{
+         get:{
+            "profile/assigned":{
+               countries:[mock_countries(3)]
+            },
+            "profile/unfulfilled":{
+               countries:[]
+            },
+            "profile/exists":{
+               profile: false 
+            },
+         },
+      },
+      result_path: "/questionaire"
+   },
+   {
+      responses:{
+         get:{
+            "profile/assigned":{
+               countries:[mock_countries(3)]
+            },
+            "profile/unfulfilled":{
+               countries:[]
+            },
+            "profile/exists":{
+               profile: true
+            },
+         },
+      },
+      result_path: "/menu"
+   },
+]
+
+respres.forEach((pair)=>{
+   let {responses,result_path}=pair
+   test(`Reaches ${result_path}`,async ()=>{
+      let api = new Api(responses)
+      let mr = new mockRouter()
+      mount(LoadingRouter,{
+         mocks:{
+            $store:{state:{api:api}},
+            $router: mr 
+         }
+      })
+      await flushPromises()
+      expect(mr.path).toMatch(new RegExp(result_path))
+   })
+})
