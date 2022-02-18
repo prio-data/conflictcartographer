dfefad7dd20f86cade554d117165556cd609ac3e
peder2911 <pglandsverk@gmail.com>
2022-02-18 17:12:24 +0100

    Fixed build step

--- app/api/apps.py
+++ app/api/apps.py
@@ -1,4 +1,4 @@
 from django.apps import AppConfig
 
-class ApiConfig(AppConfig):
+class CoreConfig(AppConfig):
     name = 'api'
--- app/cartographer/apps.py
+++ app/cartographer/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class CartographerConfig(AppConfig):
+class CoreConfig(AppConfig):
     name = 'cartographer'
