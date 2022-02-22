49b60b34ed2dfcbaf6575f0bf58d0909faacd604
peder2911 <pglandsverk@gmail.com>
2022-02-22 09:19:02 +0100

    Fixed to make 1.3.0 operational

--- app/invitations/urls.py
+++ app/invitations/urls.py
@@ -1,12 +1,11 @@
 
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path, include, re_path
 from invitations.views import referralRedirect, referralSignup, handleExcelFile,fileuploadMenu, emailpreview,share
 
 urlpatterns =[
     path("accounts/",include("django.contrib.auth.urls")),
-    url("accounts/ref/(?P<refkey>[^/.]+)/$",referralRedirect, name="referral"),
-    url("accounts/signup",referralSignup, name="referralSignup"),
+    re_path("accounts/ref/(?P<refkey>[^/.]+)/$",referralRedirect, name="referral"),
+    re_path("accounts/signup",referralSignup, name="referralSignup"),
     #path("accounts/signup",signup,name = "signup"),
     #path("accounts/bulkadd/",bulkAdd,name="bulkadd"),
     path("upload/invexcel/", handleExcelFile, name = "uploadexcel"),
--- app/unsubscribe/views.py
+++ app/unsubscribe/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.http.response import JsonResponse
 from django.forms.models import model_to_dict
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 
 from cc_backend_lib.clients import users_client
@@ -17,11 +17,13 @@ logger = logging.getLogger(__name__)
 client = users_client.UsersClient(settings.API_URL, "users")
 
 def unsubscribe_me(request: HttpRequest) -> HttpResponse:
+
     try:
-        assert (key := request.GET.get("key")) is not None
+        assert (key := request.GET.get("key"))
         email = base64.b16decode(key.encode()).decode()
         assert (user := User.objects.filter(email = email).first()) is not None
         user_id = user.id 
+
     except (AssertionError, binascii.Error) as err:
         if request.user.is_authenticated:
             user_id = request.user.id
@@ -29,4 +31,7 @@ def unsubscribe_me(request: HttpRequest) -> HttpResponse:
             return redirect("/") 
 
     result = asyncio.run(client.set_email_subscription_status(user_id, True))
-    return result.either(lambda e: Response(str(e), status = 500), lambda r: JsonResponse(r.dict()))
+
+    return result.either(
+            lambda _: render(request, "unsubscribe/failure.html.j2"),
+            lambda _: render(request, "unsubscribe/success.html.j2"))
